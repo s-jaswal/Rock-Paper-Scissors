129636bd99091732530d28298165a0d372fa4cb6
S-Jaswal <sjasdocs@gmail.com>
2024-02-14 13:49:05 -0500

    Footer, header, and onclick event

---
+++ script.js
@@ -0,0 +1,17 @@
+function userInput {}
+    const question = prompt('Ask the Magic 8-Ball a question:');
+
+    if (question === null) {
+        document.getElementById('response-text').innerText = 'Please ask a question!'
+        document.getElementById('response-text').classList = 'text-danger'
+        document.getElementById('response-image').src = '#BAD/BROKENIMAGE' //FIX THIS
+        return
+    }
+
+    if (!question.trim()) {
+        alert('Please enter a valid question!')
+        return
+
+
+}
+   
