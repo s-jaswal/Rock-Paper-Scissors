354901098d742e3a12616834162451e801a06e17
S-Jaswal <sjasdocs@gmail.com>
2024-02-20 13:44:03 -0500

    Fixed User Name Button

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-function userInput {}
-    const question = prompt('Ask the Magic 8-Ball a question:');
-
-    if (question === null) {
-        document.getElementById('response-text').innerText = 'Please ask a question!'
-        document.getElementById('response-text').classList = 'text-danger'
-        document.getElementById('response-image').src = '#BAD/BROKENIMAGE' //FIX THIS
-        return
+function userInput() {
+    var name = prompt("Please enter your name:");
+    if (name !== null) {
+        document.getElementById("response-text").innerText = "Hello, " + name + "!";
     }
-
-    if (!question.trim()) {
-        alert('Please enter a valid question!')
-        return
-
-
 }
-   
