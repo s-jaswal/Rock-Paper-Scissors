fab2b237f8a6fd68a914910dfdcc1deebd227f08
S-Jaswal <sjasdocs@gmail.com>
2024-02-27 14:59:43 -0500

    JS done ish

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+console.log()
+
+// onclick user name button start //
 function userInput() {
     var name = prompt("Please enter your name:");
     if (name !== null) {
         document.getElementById("response-text").innerText = "Hello, " + name + "!";
     }
 }
+// onclick user name button end // 
+
+let playerScore = 0;
+let computerScore = 0;
+let computerChoice;
+let playerChoice;
+
+// Add event listeners to buttons here
